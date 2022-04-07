321cbb654fb6fa5fe9efd1cb7f2c87690da5a55b
Mike <mikebio@gmail.com>
2022-04-07 21:10:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+x = -11
+
+
+if x >= 0: 
+  if x % 2 == 0: 
+    print("x is even") 
+  else: 
+    if x % 3 == 0: 
+      print("x is odd and divisible by 3")
+    else:
+      print("x is odd")
+else:
+  print("x is negative")
