963d1cd508584e2e5fd582eb0aa6ec67780af0ff
Mike <mikebio@gmail.com>
2022-04-14 20:44:05 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-x = -11
+import random
 
+def guess_number(random_number, top_range):
+  guess = int(input("Guess a number between 0 and " + str(top_range) + ": "))
 
-if x >= 0: 
-  if x % 2 == 0: 
-    print("x is even") 
-  else: 
-    if x % 3 == 0: 
-      print("x is odd and divisible by 3")
+  if guess < random_number:
+    print("Too low")
+    guess_number(random_number, top_range)
+  elif guess > random_number:
+    print ("Too high")
+    guess_number(random_number, top_range)
+  else:
+    print ("You got it!")
+    play_again = input("Would you like to play again? (Y or N)")
+
+    if (play_again == "Yes" or play_again == "yes" or play_again == "Y" or play_again == "y" or play_again == "YES"):
+      play_game()
     else:
-      print("x is odd")
-else:
-  print("x is negative")
+      print ("Thank you! Good bye!")
+
+    
+def play_game():
+  top_range = int(input("Enter a number: "))
+  
+  if (top_range < 1):
+    print ("Number must be greater than 0")
+    play_game()
+    
+  random_number = random.randint(0, top_range)
+  guess_number(random_number, top_range)
+
+play_game()
